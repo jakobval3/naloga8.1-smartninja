bbc210d58f55c8c7abf26f287124f2bad7061fcc
tilenavs <tilenavs@gmail.com>
2022-09-29 18:57:39 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+mood= "happy"
+if mood== "happy" :
+    print("It's great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood== "sad":
+    print("Cheer up buttercup :).")
+elif mood== "excited":
+    print("I'm glad to see you hyped!")
+elif mood== "relaxed":
+    print("Your vibez are immaculate!")
+else:
+    print("I don't recognize this mood...")
